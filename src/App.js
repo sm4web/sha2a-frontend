e620cbebcef2af12328ab7c324e1875232898646
saifmohamedsv <xcyberghostx1@gmail.com>
2022-07-11 11:11:24 +0200

    'update'

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
 import SideWrapper from "./components/SideWrapper/SideWrapper";
 import Login from "./pages/login/login.jsx";
-import ProtectedRoute from "./components/protectedRoute/reverseProtectedRoute";
+import ProtectedRoute from "./components/handleAuth/protectedRoute";
 import Register from "./pages/register/register";
 import CompleteRegister from "./pages/completeRegister/completeRegister";
 
@@ -19,11 +19,10 @@ function App() {
                     <Route exact path={"/register"} element={<Register/>}/>
                     <Route exact path={"/personal-info"} element={<CompleteRegister/>}/>
                     <Route element={<ProtectedRoute/>}>
-
                         <Route path={"/home"} element={<SideWrapper/>}>
                             <Route exact path={"user"} element={<h1>user</h1>}/>
                         </Route>
-                        <Route exact path={"/"}/>
+                        <Route exact index path={"/"}/>
                     </Route>
                 </Routes>
             </BrowserRouter>
