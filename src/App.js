5e5044cd81ba66ea1e81cc340c673af897adfbc1
saifmohamedsv <xcyberghostx1@gmail.com>
2022-07-19 13:45:10 +0200

    'update'

--- src/App.js
+++ src/App.js
@@ -9,34 +9,36 @@ import CompleteRegister from "./pages/completeRegister/completeRegister";
 import SideNav from "./components/side-nav/sideNav.tsx";
 import Home from "./pages/home/Home";
 import CreateAdvertisement from "./pages/createAdvertisement/createAdvertisement";
-import AdvertisementPage from "./pages/advertisementPage/advertisementPage";
-
+// import AdvertisementPage from "./pages/advertisementPage/advertisementPage";
+const AdvertisementPage = React.lazy(() => import("./pages/advertisementPage/advertisementPage"))
 
 function App() {
     return (
         <div className="App">
-            <BrowserRouter>
-                <Routes>
+            <React.Suspense fallback={<div>Loading...</div>}>
+                <BrowserRouter>
+                    <Routes>
 
-                    <Route element={<ProtectedRoute/>}>
-                        <Route path={"/"} element={<Layout/>}>
-                            <Route index element={<Home/>}/>
-                            <Route path={"create-ad"} element={<CreateAdvertisement/>}/>
-                            <Route path={"advertisements/:id"} element={<AdvertisementPage/>}/>
-                            <Route path={"favorites"} element={<h1>Favorites</h1>}/>
-                            <Route path={"settings"} element={<h1>Settings</h1>}/>
-                            <Route path={"my-ads"} element={<h1>My Ads</h1>}/>
+                        <Route element={<ProtectedRoute/>}>
+                            <Route path={"/"} element={<Layout/>}>
+                                <Route index element={<Home/>}/>
+                                <Route path={"create-ad"} element={<CreateAdvertisement/>}/>
+                                <Route path={"advertisements/:id"} element={<AdvertisementPage/>}/>
+                                <Route path={"favorites"} element={<h1>Favorites</h1>}/>
+                                <Route path={"settings"} element={<h1>Settings</h1>}/>
+                                <Route path={"my-ads"} element={<h1>My Ads</h1>}/>
+                            </Route>
                         </Route>
-                    </Route>
 
-                    {/*  START -- All the register steps  */}
-                    <Route exact path={"/login"} element={<Login/>}/>
-                    <Route exact path={"/register"} element={<Register/>}/>
-                    <Route exact path={"/personal-info"} element={<CompleteRegister/>}/>
-                    {/*  END -- All the register steps  */}
+                        {/*  START -- All the register steps  */}
+                        <Route exact path={"/login"} element={<Login/>}/>
+                        <Route exact path={"/register"} element={<Register/>}/>
+                        <Route exact path={"/personal-info"} element={<CompleteRegister/>}/>
+                        {/*  END -- All the register steps  */}
 
-                </Routes>
-            </BrowserRouter>
+                    </Routes>
+                </BrowserRouter>
+            </React.Suspense>
         </div>
     );
 }
